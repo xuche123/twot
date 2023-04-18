50add91e2f9836c28906f3055eadbe5abf83ade9
Xu Che <xuche1998@gmail.com>
2023-04-18 14:31:52 +0800

    Header and Followbar implemented

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router"
+import { RxArrowLeft } from "react-icons/rx"
+
+type Props = {
+  label: string
+  back?: boolean
+}
+
+const Header = ({label, back}: Props) => {
+  const router = useRouter()
+  const handleBack = () => {
+    router.push("/")
+  }
+  return (
+    <div className="border-b-[1px] border-neutral-800 p-5">
+      <div className="flex flex-row items-center gap-2">
+        {back && (
+          <div onClick={handleBack} className="cursor-pointer transition hover:opacity-70">
+            <RxArrowLeft size={28} color="white" />
+          </div>
+        )}
+      <h1 className="text-white text-semibold text-xl">{label}</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className='text-sky-500 text-3xl'>Hello World</div>
+    <>
+      <Header label="Home" />
+    </>
   )
 }
